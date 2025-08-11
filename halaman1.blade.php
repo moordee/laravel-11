591e0c0d42aecc6cd583f7b61e5ce27c5b4665e4
Scientia Hashina <srsshashina@gmail.com>
2025-08-11 14:09:48 +0700

    located in laravel-project-folder/views

---
+++ halaman1.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Rumah</title>
+</head>
+<body>
+    Selamat datang di Laravel 11. <br>
+    Ini adalah halaman 1
+
+    <a href="/halaman2">Lanjut ke Halaman 2</a><br>
+    <a href="/halaman3">Lompat ke Halaman 3</a><br>
+    <a href="/halaman4">Lompat ke Halaman 4</a>
+</body>
+</html>
+
